b43a32bfd7b7eb0fe5383a1e07b7402940394510
Winslow987 <72766592+Winslow987@users.noreply.github.com>
2022-03-31 14:27:34 +0200

    Update TP3.sh

--- TP3.sh
+++ TP3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "pct create "'$var_id'" local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz --hostname "'$var_name'" --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip="'$var_ip'"/24,gw=192.168.56.254 --nameserver 1.1.1.1 --password "'$var_password'
+pct create $3 local:vztmpl/debian-11-standard_11.0-1_amd64.tar.gz --hostname $1 --cores 1 --memory 512 --swap 512 --rootfs local-lvm:5 --net0 name=eth0,bridge=vmbr0,ip=$2/24,gw=192.168.1.201 --nameserver 1.1.1.1 --start 1 --password $4 
